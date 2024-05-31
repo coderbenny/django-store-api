9187c312854541925d22eb203d06b3a3b990b183
benny <benny.mathew@student.moringaschool.com>
2024-05-31 23:24:17 +0300

    added the urls

--- store_app/urls.py
+++ store_app/urls.py
@@ -5,6 +5,8 @@ from  . import views
 app_name = 'store_app'
 urlpatterns = [
     path("", views.Index, name="Index"),
+    path("clients/", views.get_clients, name="Clients"),
+    path("items/", views.get_items, name="Items"),
     path('order/', views.create_order, name='create_order'),
     path('total_revenue/', views.get_total_revenue, name='total_revenue'),
     path('total_sales/', views.get_total_sales, name='total_sales'),

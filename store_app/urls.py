d92bfd761152d89467647d9d4c0a1f5e91a3159d
benny <benny.mathew@student.moringaschool.com>
2024-06-01 15:31:45 +0300

    added readme

--- store_app/urls.py
+++ store_app/urls.py
@@ -4,11 +4,13 @@ from  . import views
 
 app_name = 'store_app'
 urlpatterns = [
-    path("", views.Index, name="Index"),
-    path("clients/", views.get_clients, name="Clients"),
-    path("items/", views.get_items, name="Items"),
-    path('order/', views.create_order, name='create_order'),
-    path('total_revenue/', views.get_total_revenue, name='total_revenue'),
-    path('total_sales/', views.get_total_sales, name='total_sales'),
-    path('items/<int:item_id>/', views.view_item, name='view_item'),
+    path("", views.Index, name="Index"), # Index Route
+    path("clients/", views.get_clients, name="Clients"), # All Clients Route
+    path("items/", views.get_items, name="Items"), # All Items Route
+    path("sales/", views.all_sales, name="sales"), # All Sales Route
+    path('order/', views.create_order, name='create_order'), # Route for Creating an Order
+    path('total-revenue/', views.get_total_revenue, name='total_revenue'), # Total Revenue Route
+    path('total-sales/', views.get_total_sales, name='total_sales'), # Total Sales Route
+    path('items/view/<int:item_id>/', views.view_item, name='view_item'), # Item By ID Route
+    path('items/<int:item_id>/', views.delete_item, name='delete_item'), # Route for Deleting Item
 ]
--- store_app/views.py
+++ store_app/views.py
@@ -10,7 +10,7 @@ from .serializers import ClientSerializer, ItemSerializer, SaleSerializer
 # Index route
 @api_view(['GET'])
 def Index(request):
-    return Response({"Home":"Welcome to the store app"}, status=status.HTTP_200_OK)
+    return Response({"Home":"Welcome to the store api"}, status=status.HTTP_200_OK)
 
 # All Clients route
 @api_view(['GET'])
@@ -32,9 +32,16 @@ def delete_item(request, item_id):
     try:
         item = Item.objects.get(id=item_id)
         item.delete()
-        return Response({'message':'Item deleted succesfully'}, status=status.HTTP_204_NO_CONTENT)
-    except item.DoesNotExist:
-        return Response({'error':'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'message': 'Item deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    except Item.DoesNotExist:  
+        return Response({'error': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+
+# All Sales Route
+@api_view(['GET'])
+def all_sales(request):
+    sales = Sale.objects.all()
+    serializer = SaleSerializer(sales, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 # View Item By ID
 @api_view(['GET'])
@@ -54,10 +61,10 @@ def create_order(request):
     item_id = request.data.get('item_id')
 
     if not client_name or not item_id:
-        return Response({"error": "Client data and item ID are required"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "Client name and item ID are required"}, status=status.HTTP_400_BAD_REQUEST)
 
     # Handle client creation or retrieval
-    client, created = Client.objects.get_or_create(email=client_name['name'], defaults={'name': client_name})
+    client, created = Client.objects.get_or_create(name=client_name, defaults={'name': client_name})
 
     try:
         item = Item.objects.get(id=item_id)
@@ -65,9 +72,10 @@ def create_order(request):
         return Response({"error": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
 
     # Create sale
-    sale = Sale.objects.create(client=client_name, item=item)
+    sale = Sale.objects.create(client=client, item=item)
 
     return Response({"message": "Order created successfully", "sale": SaleSerializer(sale).data}, status=status.HTTP_201_CREATED)
+
 
 # Total Revenue Route
 @api_view(['GET'])

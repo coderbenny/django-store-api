86d615011f00f7b187f72a17c40aff257958d045
benny <benny.mathew@student.moringaschool.com>
2024-06-11 23:27:37 +0300

    installed gunicorn

--- store_app/models.py
+++ store_app/models.py
@@ -13,6 +13,7 @@ class Client(models.Model):
 class Item(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    sold = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
